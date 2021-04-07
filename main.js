f88596048e03acb05dcf08e408e63f2b08d62f88
lorena andrade <lorenasandrade.silva@gmail.com>
2021-04-07 12:40:48 -0300

    primeiro commit

---
+++ main.js
@@ -0,0 +1,9 @@
+var QualHokage =  alert("Qual Hokage é filho do Minato e abriga a raposa de 9 caldas? ")
+var opcoes = (prompt('Digite 1 para Hashirama, 2 para Tobirama, 3 Sarutobi, 4 para Minato, 5 para Tsunade, 6 para Kakashi e 7 para Naruto: '))
+
+if (opcoes ==7){
+    var resultado= 7
+    document.write ("<h2>Você acertou! Naruto Uzumaki.</h2>")
+} else {
+    document.write ("<h2>Opçao Inválida,Tente novamente.</h2>")
+}
